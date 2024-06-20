3c87b58d7df189781ed677d91ca933ccbb0eefe7
Sparsh Nidhi <sparsh123nidhi@gmail.com>
2024-06-20 21:59:04 +0545

    added comments

--- ny.py
+++ ny.py
@@ -2,22 +2,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-x=np.array([1,2,3,4])
-y= np.array([1,2,3,4])
-m=x.shape[0]
-print(m)
+x=np.array([1,2,3,4]) #This is the input features
+y= np.array([1,2,3,4]) #This is the training set 
+m=x.shape[0] #Stores the length of x in m
 
-f=np.zeros(m)
+f=np.zeros(m) #creates a numpy array and initializes it with zeroes
 for i in range (m):
-    f[i]=w*x[i]+b
+    f[i]=w*x[i]+b #linear regression model, f is the model's prediction
 sum=0
 for i in range(m):
-    sum=sum + ( f[i]-y[i])**2
-j=(1/(2*m))*sum
+    sum=sum + ( f[i]-y[i])**2 
+j=(1/(2*m))*sum #j is the cost
 print(j)
 
-plt.plot(x,y,marker="x", label="try")
-plt.plot(x,f,marker="o", c= "r")
+plt.plot(x,y,marker="x", label="try") #plot of x vs y
+plt.plot(x,f,marker="o", c= "r") #plot of the linear regression model
 plt.legend()
 plt.show()
 
