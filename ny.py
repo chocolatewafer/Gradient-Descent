eff41b94aeb777ead21d9f1d544d83dbc2516c83
Sparsh Nidhi <sparsh123nidhi@gmail.com>
2024-07-06 12:36:36 +0545

    added compute gradient function

--- ny.py
+++ ny.py
@@ -16,9 +16,42 @@ def compute_cost(x,y,w,b): #funciton to compute cost
     print("cost:", j)
     return j
 
-w, b=1, 1
+def compute_gradient(x,y,w,b):
+    m=x.shape[0]
+    dj_dw = 0
+    dj_db = 0
+    
+    for i in range(m):
+        f=w*x[i]+b
+        dj_db_i=f-y[i]
+        dj_dw_i=(f- y[i] )*x[i]
+        dj_db += dj_db_i 
+        dj_dw= dj_dw_i
+    dj_db=dj_db/m
+    dj_dw=dj_dw/m
+    print(dj_db, dj_dw)
+    return dj_dw, dj_db
+
+    
+
+
+
+
+
+
+
+
+w, b=8, 3
+
 compute_cost(x,y,w,b)
+compute_gradient(x,y,w,b)
+
+
+
+
+
 
 plt.plot(x,y,marker="x", label="try") #plot of x vs y
 plt.legend()
 plt.show()
+
